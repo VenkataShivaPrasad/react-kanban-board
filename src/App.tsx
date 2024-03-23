42580ebb7ce5eda3d165143632022ec1160b7cf6
Venkata Shiva Prasad <venkatashivaprasad.nannuri@gmail.com>
2024-03-23 16:12:54 +0530

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import './App.css'
 const App = () => {
   return (
     <div>
+      <h1
+       className=" text-2xl font-semibold text-center mt-10 leading-3"
+      >Kanban Board</h1>
       <KanbanBoard/>
     </div>
   )
